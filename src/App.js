3f6049d66b5a17cf74decaac264364511f6bec15
nati hadad <85611969+NatiHadad007@users.noreply.github.com>
2024-06-12 15:44:00 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./social-files/main";
 import Login from "./social-files/logIn";
 import Signin from "./social-files/signIn";
+import ProfileBanner from "./social-files/profileBanner";
 import "./App.css";
-
+import AuthProvider from "./context/AuthProvider";
+import PostsProvider from "./context/PostsProvider";
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/log-in" element={<Login />} />
-        <Route path="/sign-in" element={<Signin />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <PostsProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/log-in" element={<Login />} />
+            <Route path="/sign-in" element={<Signin />} />
+            <Route path="/profile-banner" element={<ProfileBanner />} />
+          </Routes>
+        </div>
+      </PostsProvider>
+    </AuthProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCQhqQRsCcIUmGRPihHFSCV8BUILxr407c",
@@ -17,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const storage = getStorage(app);
 
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
